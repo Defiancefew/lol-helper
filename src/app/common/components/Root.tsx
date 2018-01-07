3ca9ea04e0706d55ccd1c5e47a332fe51f4abc3a
Defiancefew <Severancepwns@gmail.com>
2018-01-07 21:38:00 +0300

    added redux-persist & persist configuration

--- src/app/common/components/Root.tsx
+++ src/app/common/components/Root.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import { hot } from 'react-hot-loader';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import { Routes } from './Routes';
+import { ConnectedModal } from '../../auth/components';
 
-export const Root: React.SFC<any> = ({ history, store }) => (
+export const Root: React.SFC<any> = ({ history, store, persistor, onBeforeLift }) => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Routes />
+      <PersistGate persistor={persistor} onBeforeLift={onBeforeLift}>
+        <ConnectedModal />
+        <Routes />
+      </PersistGate>
     </ConnectedRouter>
   </Provider>
 );
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,10 +3,15 @@ import ReactDom from 'react-dom';
 import Root from './common/components/Root';
 import { history, configureStore } from './common/configureStore';
 import 'antd/dist/antd.less'; // tslint:disable-line:no-submodule-imports
+import { checkKey } from './auth/actions';
 
-const store = configureStore();
+const { persistor, store } = configureStore();
 const root = document.createElement('div');
+
+const onBeforeLift = () => {
+  store.dispatch(checkKey(store.getState().auth.apiKey));
+};
 
 document.body.appendChild(root);
 
-ReactDom.render(<Root store={store} history={history} />, root);
+ReactDom.render(<Root store={store} history={history} persistor={persistor} onBeforeLift={onBeforeLift} />, root);
