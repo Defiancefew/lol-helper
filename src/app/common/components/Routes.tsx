37b24ca7ac534bf2b6835024cae18a49a61d2a4d
Defiancefew <Severancepwns@gmail.com>
2018-01-09 18:17:25 +0300

    configured routes

--- src/app/common/components/Routes.tsx
+++ src/app/common/components/Routes.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { App, DevTools, AppLayout, NoMatch } from './';
 import { AuthForm } from '../../auth/components';
+import { ConnectedSearch as Summoner, SummonerProfilePage, MatchPage } from '../../summoner/components';
 
 export const Routes = () => (
   <AppLayout>
@@ -10,6 +11,10 @@ export const Routes = () => (
     <Switch>
       <Route exact path="/" component={App} />
       <Route path="/keycheck" component={AuthForm} />
+      <Route path="/summoner/match/:matchid" component={MatchPage} />
+      <Route path="/summoner/:profile" component={SummonerProfilePage} />
+      <Route path="/summoner" component={Summoner} />
+      {/* <Route path="/itemset" component={ItemSet} /> */}
       <Route component={NoMatch} />
     </Switch>
   </AppLayout>
