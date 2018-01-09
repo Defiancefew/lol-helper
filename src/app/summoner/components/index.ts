3f7303abd600ee56ee0f80bde25e45b9da00ac33
Defiancefew <Severancepwns@gmail.com>
2018-01-09 18:19:22 +0300

    added index file for summoner

---
+++ src/app/summoner/components/index.ts
@@ -0,0 +1,6 @@
+export * from './SummonerSearch';
+export * from './SearchHistoryProfile';
+export * from './MatchListItem';
+export * from './MatchTableLine';
+export { ConnectedProfilePage as SummonerProfilePage } from './SummonerProfilePage';
+export { ConnectedMatchPage as MatchPage } from './MatchProfilePage';
