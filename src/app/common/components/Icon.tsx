34c190a1c80e05e79e6b2b4ece5979f328de92b7
Defiancefew <Severancepwns@gmail.com>
2018-01-09 18:23:27 +0300

    icon improvement

--- src/app/common/components/Icon.tsx
+++ src/app/common/components/Icon.tsx
@@ -2,20 +2,37 @@ import React from 'react';
 import styled from 'styled-components';
 import { backgroundStyle } from 'utils';
 import _ from 'lodash';
+import * as iconData from 'static';
 
 export interface IIconProps {
   ref?: any;
   id: string | number;
-  type: 'champion' | 'item' | 'spell' | 'profileicon';
+  type: 'champion' | 'item' | 'spell' | 'profileicon' | 'summoner';
   tooltip?: boolean;
 
   onClick?(...args: any[]): void;
 }
 
-export const Icon: React.SFC<IIconProps> = ({ type, id }) => {
-  const iconData = require(`static`);
-  const spriteData = _.get(iconData, `[${type}].data[${id}]`, {});
-  return <StyledIcon background={backgroundStyle(spriteData.image)} />;
+const findSpritePath = (type: string, id: string | number) => {
+  if (_.includes(['champion', 'summoner'], type)) {
+    return `[${type}].data[${type === 'champion' ? iconData.championMap[id] : iconData.summonerMap[id]}]`;
+  }
+
+  return `[${type}].data[${id}]`;
+};
+
+export const Icon: React.SFC<IIconProps> = ({ type, id, onClick }) => {
+  const spriteData = _.get(iconData, findSpritePath(type, id), {
+    image: {
+      type: 'profileicon',
+      sprite: 'profileicon0.png',
+      id: '0',
+      x: '0',
+      y: '0',
+    },
+  });
+
+  return <StyledIcon onClick={onClick} background={backgroundStyle(spriteData.image)} />;
 };
 
 export const StyledIcon = styled.div`
