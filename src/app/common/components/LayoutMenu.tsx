5e47dfc099bbc6115889d941029f32f5d4bb484e
Defiancefew <Severancepwns@gmail.com>
2018-01-10 08:02:25 +0300

    quick layout fix

--- src/app/common/components/LayoutMenu.tsx
+++ src/app/common/components/LayoutMenu.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import { Menu } from 'antd';
+import { Menu, Icon } from 'antd';
 import { Link } from 'react-router-dom';
 
 export const LayoutMenu: React.SFC<any> = () => (
-  <Menu theme="dark" mode="horizontal">
+  <Menu style={{ display: 'flex', lineHeight: '64px' }} theme="dark" mode="horizontal">
     <Menu.Item>
       <Link to="/">Home</Link>
-    </Menu.Item>
-    <Menu.Item>
-      <Link to="/keycheck">Auth</Link>
     </Menu.Item>
     <Menu.Item>
       <Link to="/summoner">Summoner</Link>
     </Menu.Item>
     <Menu.Item>
       <Link to="/itemset">Item set</Link>
+    </Menu.Item>
+    <Menu.Item>
+      <Link to="/rune">Rune calc</Link>
+    </Menu.Item>
+
+    <Menu.Item style={{ marginLeft: 'auto' }}>
+      <Link to="/keycheck">
+        <Icon type="login" />
+      </Link>
     </Menu.Item>
   </Menu>
 );
