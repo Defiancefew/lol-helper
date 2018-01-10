53a769a865ccb0fa7d64abb1450123d7c9c03630
Defiancefew <Severancepwns@gmail.com>
2018-01-10 07:52:37 +0300

    items list component

---
+++ src/app/itemset/components/ItemsList.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import _ from 'lodash';
+import { DraggableIcon } from './';
+import { IItem } from 'models';
+
+const ItemsWrapper = styled.div`
+  display: flex;
+  flex-flow: wrap;
+  max-width: 800px;
+  height: 780px;
+  padding-bottom: 400px;
+  margin-top: 16px;
+`;
+
+export class ItemsList extends React.Component<{ items: { data: { [key: string]: IItem } } }, any> {
+  render() {
+    const { items } = this.props;
+    return (
+      <ItemsWrapper>
+        {_.map(items.data, (item: IItem, id: string) => <DraggableIcon key={id} type="item" id={id} />)}
+      </ItemsWrapper>
+    );
+  }
+}
