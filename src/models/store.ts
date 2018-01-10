962d565cafe092300ae9971b3cdb132fb92675a2
Defiancefew <Severancepwns@gmail.com>
2018-01-10 07:51:33 +0300

    sprite & item set models

--- src/models/store.ts
+++ src/models/store.ts
@@ -1,9 +1,13 @@
 import { RouterState } from 'react-router-redux';
 import { IApiReducer } from '../app/auth/reducer';
 import { ISummonerReducerState } from '../app/summoner/reducer';
+import { IItemSetState } from '../app/itemset/reducer';
+import { IRuneState } from '../app/rune/reducer';
 
 export interface IStore {
   routing: RouterState;
   auth: IApiReducer;
   summoner: ISummonerReducerState;
+  itemset: IItemSetState;
+  rune: IRuneState;
 }
