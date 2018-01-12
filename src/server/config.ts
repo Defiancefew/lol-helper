d02c5a9cf700b4cbae1bb2e4378849bd59d61da0
Defiancefew <Severancepwns@gmail.com>
2018-01-12 04:38:55 +0300

    update static data & bump datadragon version

--- src/server/config.ts
+++ src/server/config.ts
@@ -2,4 +2,4 @@ export const apiUrl = 'api.riotgames.com/lol/';
 export const configUrl = (region: string, type: string) => `https://${region}.${apiUrl}${type}`;
 export const dataDragonUrl = (version: string, type: string, url: string) =>
   `http://ddragon.leagueoflegends.com/cdn/${version}/${type}/${url}`;
-export const dataDragonVersion = '6.24.1';
+export const dataDragonVersion = '7.24.2';
