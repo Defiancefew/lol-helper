b25f419c8429553ada891cbfcb96c32dbc7d7c3f
Defiancefew <Severancepwns@gmail.com>
2018-01-12 04:32:12 +0300

    added rune reset

--- src/app/rune/actions.ts
+++ src/app/rune/actions.ts
@@ -3,6 +3,7 @@ import { createAction } from 'redux-act';
 const NS = '@@RUNE/';
 
 export const addRune = createAction(`${NS} ADD RUNE`);
+export const removeRune = createAction(`${NS} REMOVE RUNE`);
 export const selectPrimary = createAction(`${NS} SELECT PRIMARY`);
 export const selectSecondary = createAction(`${NS} SELECT SECONDARY`);
 export const resetRune = createAction(`${NS} RESET RUNE`);
