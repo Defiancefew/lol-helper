459d6793a12997cd79bbe9c2be1b90fd278a1a05
Defiancefew <Severancepwns@gmail.com>
2018-01-14 03:20:54 +0300

    league interface & mocks

--- src/models/summoner.ts
+++ src/models/summoner.ts
@@ -6,3 +6,20 @@ export interface ISummonerInfo {
   revisionDate: number;
   summonerLevel: number;
 }
+
+export interface ISummonerLeague {
+  queueType: string;
+  hotStreak: boolean;
+  wins: number;
+  veteran: boolean;
+  losses: number;
+  playerOrTeamId: string;
+  leagueName: string;
+  playerOrTeamName: string;
+  inactive: boolean;
+  rank: string;
+  freshBlood: boolean;
+  leagueId: string;
+  tier: string;
+  leaguePoints: number;
+}
