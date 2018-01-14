1a2eeb024ae5fd042533461066bb1f323dd3a3b2
Defiancefew <Severancepwns@gmail.com>
2018-01-15 02:04:07 +0300

    fixed type in tsconfig & added alias

--- webpack/resolve.js
+++ webpack/resolve.js
@@ -3,5 +3,5 @@ const mainDir = path.resolve(__dirname, '../');
 
 module.exports = {
   extensions: ['.js', '.json', '.ts', '.tsx'],
-  modules: [path.join(mainDir, 'src'), 'node_modules'],
+  modules: [path.join(mainDir, 'src'), 'node_modules', path.join(mainDir, 'src', 'app')],
 };
