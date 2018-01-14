3db36a84feba4b89368b739474b417cef168b959
Defiancefew <Severancepwns@gmail.com>
2018-01-15 02:05:59 +0300

    added modal to layout and navbar

--- src/app/common/components/Layout.tsx
+++ src/app/common/components/Layout.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import { Layout } from 'antd';
 import styled from 'styled-components';
 import { withRouter } from 'react-router-dom';
-import { LayoutMenu } from './';
+import { ConnectedMenu } from './';
 
 const { Header, Content, Footer } = Layout;
 
+const StyledLayout = styled(Layout)`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const StyledContent = styled(Content)`
+  padding: 50px;
+  min-height: 100vh;
+`;
+
 export const MainLayout: React.SFC<{}> = ({ children }) => (
-  <Layout style={{ maxWidth: '1200px', margin: '0 auto' }}>
+  <StyledLayout>
     <Header>
-      <LayoutMenu />
+      <ConnectedMenu />
     </Header>
-    <Content style={{ padding: '50px', minHeight: '100vh' }}>{children}</Content>
-  </Layout>
+    <StyledContent>{children}</StyledContent>
+  </StyledLayout>
 );
 
 export const AppLayout = withRouter(MainLayout);
