25c7aa3f9db83342996b503e08b723994155cc3a
Defiancefew <Severancepwns@gmail.com>
2018-01-20 20:05:58 +0300

    added thread-loader, cache-loader and babel-loader to webpack rules

--- webpack/rules.js
+++ webpack/rules.js
@@ -1,23 +1,36 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = env => {
-  const rules = {
-    ts: {
-      test: /\.tsx?$/,
-      use: ['awesome-typescript-loader'],
-      exclude: /node_modules/,
+exports.ts = {
+  test: /\.tsx?$/,
+  use: [
+    { loader: 'cache-loader' },
+    {
+      loader: 'thread-loader',
+      options: {
+        // there should be 1 cpu for the fork-ts-checker-webpack-plugin
+        workers: require('os').cpus().length - 1,
+      },
     },
-    less: {
-      test: /\.less$/,
-      use: env.production
-        ? ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: ['css-loader', 'less-loader'],
-          })
-        : ['style-loader', 'css-loader', 'less-loader'],
-      include: /node_modules/,
+    {
+      loader: 'babel-loader',
     },
-  };
-
-  return [rules.ts, rules.less];
+    {
+      loader: 'ts-loader',
+      options: {
+        happyPackMode: true, // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
+      },
+    },
+  ],
+  exclude: /node_modules/,
 };
+
+exports.less = env => ({
+  test: /\.less$/,
+  use: env.production
+    ? ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'less-loader'],
+      })
+    : ['style-loader', 'css-loader', 'less-loader'],
+  include: /node_modules/,
+});
