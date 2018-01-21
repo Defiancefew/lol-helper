a083ffd2255bac4e18249d83a31eaebd6d162093
Defiancefew <Severancepwns@gmail.com>
2018-01-22 00:17:12 +0300

    redirect to new match route

--- src/app/summoner/components/MatchListItem.tsx
+++ src/app/summoner/components/MatchListItem.tsx
@@ -6,6 +6,7 @@ import { Icon } from 'common';
 const ItemWrapper = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-around;
   padding: 10px;
   margin: 5px 0;
   border: 1px solid gray;
@@ -17,11 +18,10 @@ const ItemWrapper = styled.div`
 
 const DescWrapper = styled.span`
   width: 200px;
-  margin-left: 10px;
 `;
 
 export class MatchListItem extends React.Component<any, any> {
-  handleClick = () => this.props.push(`/summoner/match/${this.props.gameId}`);
+  handleClick = () => this.props.push(`/match/${this.props.gameId}`);
 
   render() {
     const { champion, timestamp, lane, queue } = this.props;
